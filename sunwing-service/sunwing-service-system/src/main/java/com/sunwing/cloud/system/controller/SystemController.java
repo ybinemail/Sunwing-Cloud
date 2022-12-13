b8bacb14cea5b25e1e225de09c33767c112d8d97
yubin <yubin@unisound.com>
2022-12-13 13:29:41 +0800

    添加引用

--- sunwing-service/sunwing-service-system/src/main/java/com/sunwing/cloud/system/controller/SystemController.java
+++ sunwing-service/sunwing-service-system/src/main/java/com/sunwing/cloud/system/controller/SystemController.java
@@ -1,6 +1,9 @@
 package com.sunwing.cloud.system.controller;
 
 import com.sunwing.cloud.system.service.ISystemService;
+import com.sunwing.platform.boot.common.base.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,18 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "system")
 @AllArgsConstructor
-
+@Api(tags = "SunWing-System")
 public class SystemController {
-    private final ISystemService iSystemService;
-
+    private final ISystemService systemService;
     @GetMapping(value = "list")
+    @ApiOperation("获取list")
     public Object list(){
-        return iSystemService.list();
+        return systemService.list();
     }
 
     @GetMapping(value = "page")
+    @ApiOperation("获取list")
     public Object page(){
-        return iSystemService.page();
+        return systemService.page();
+    }
+
+    @GetMapping(value = "mockException")
+    @ApiOperation("自定义异常")
+    public Result<String> mockException(){
+        return Result.data(systemService.mockException());
     }
 
 }
--- sunwing-service/sunwing-service-system/src/main/java/com/sunwing/cloud/system/service/impl/SystemServiceImpl.java
+++ sunwing-service/sunwing-service-system/src/main/java/com/sunwing/cloud/system/service/impl/SystemServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.sunwing.cloud.system.entity.SystemTable;
 import com.sunwing.cloud.system.mapper.SystemTableMapper;
 import com.sunwing.cloud.system.service.ISystemService;
+import com.sunwing.platform.boot.common.exception.BusinessException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +26,10 @@ public class SystemServiceImpl implements ISystemService {
         List<SystemTable> records = systemTableMapper.page(page);
         page.setRecords(records);
         return page;
+    }
+
+    @Override
+    public String mockException() {
+        throw new BusinessException("自定义异常");
     }
 }
